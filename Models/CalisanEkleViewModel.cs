ce4ac3ee809d0623c24629355ce4874288f80a38
Akif Emre Sula <akifsula@gmail.com>
2024-12-22 04:22:23 +0300

    deneme

---
+++ Models/CalisanEkleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using KuaforYonetim.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace KuaforYonetim.ViewModels
+{
+    public class CalisanEkleViewModel
+    {
+        [Required]
+        [Display(Name = "Ad Soyad")]
+        public string AdSoyad { get; set; }
+
+        [Display(Name = "Uzmanlık Alanları")]
+        public List<int> SelectedHizmetler { get; set; } = new List<int>();
+
+        public List<Hizmet> Hizmetler { get; set; } = new List<Hizmet>();
+    }
+}
